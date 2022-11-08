03d6cd4fba3a62e7d8c96a4a62c0d95c7f2d00d2
aminul007bd <gem007bd@gmail.com>
2022-11-08 09:28:02 -0800

    semiclone remove

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import { HeaderPage } from './pages/HeaderPage'
+import Row from 'react-bootstrap/Row'
+import data from './data.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [personalData] = data
+	// console.log(personalData.personalInformation)
+	return (
+		<>
+			<Container>
+				<HeaderPage personalInformation={personalData.personalInformation} />
+			</Container>
+		</>
+	)
 }
 
-export default App;
+export default App
